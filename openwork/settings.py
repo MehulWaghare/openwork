ebaf7667adabe984952d65d8d201a77ba9cf6817
MehulWaghare <126313845+MehulWaghare@users.noreply.github.com>
2025-11-15 14:30:12 +0530

    Updated settings.py for Railway MySQL

--- openwork/settings.py
+++ openwork/settings.py
@@ -87,8 +87,13 @@ DATABASES = {
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
 
 
 
